24682612ac78f2efbbd05e62bf8f984a7c441426
AsusRogAlly2079 <parradosamuel35@gmail.com>
2025-10-05 19:37:00 -0500

    arreglo3.0

--- Persistencia/InicioUsuario.cs
+++ Persistencia/InicioUsuario.cs
@@ -12,14 +12,31 @@ namespace SistemaInventarioColchones.Persistencia
 {
     public partial class InicioUsuario: Form
     {
-        public InicioUsuario()
+        private string rolUsuario;
+
+        public InicioUsuario(string rol)
         {
             InitializeComponent();
-        }
+            rolUsuario = rol;
 
+            // Bloquea el botón de usuarios aquí:
+            if (rolUsuario != "administrador")
+            {
+                btnUsuarios.Enabled = false;
+                btnUsuarios.Text = "Bloqueado";
+            }
+        }
+        //InicioUsuario frm = new InicioUsuario(rolUsuario);
         private void InicioUsuario_Load(object sender, EventArgs e)
         {
+           
 
+            // Bloquea el botón aquí también (además del Load)
+            if (rolUsuario != "administrador")
+            {
+                btnUsuarios.Enabled = false;
+                btnUsuarios.Text = "Bloqueado";
+            }
         }
 
         private void label3_Click(object sender, EventArgs e)
@@ -44,21 +61,26 @@ namespace SistemaInventarioColchones.Persistencia
 
         private void btnUsuarios_Click(object sender, EventArgs e)
         {
-            UsuariosUsuarios frm = new UsuariosUsuarios();
+            if (rolUsuario != "administrador")
+            {
+                MessageBox.Show("Acceso solo permitido para administradores.", "Acceso denegado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            InicioUsuario frm = new InicioUsuario(rolUsuario);
             frm.Show();
             this.Hide();
         }
 
         private void btnProductos_Click(object sender, EventArgs e)
         {
-            ProductosUsuario frm = new ProductosUsuario();
+            ProductosUsuario frm = new ProductosUsuario("vendedor");
             frm.Show();
             this.Hide();
         }
 
         private void btnProveedores_Click(object sender, EventArgs e)
         {
-            ProveedoresUsuario frm= new ProveedoresUsuario();
+            ProveedoresUsuario frm= new ProveedoresUsuario("vendedor");
             frm.Show();
             this.Hide();
         }
@@ -77,7 +99,7 @@ namespace SistemaInventarioColchones.Persistencia
 
         private void btnReportes_Click(object sender, EventArgs e)
         {
-            InicioUsuario frm = new InicioUsuario();
+            InicioUsuario frm = new InicioUsuario(rolUsuario);
             frm.Show();
             this.Hide();
 
